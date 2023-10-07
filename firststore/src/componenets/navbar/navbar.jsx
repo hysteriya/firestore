bbc7d4a7fd2fd73c6bc138b5edb5c29c0e808700
hysteriya <126953127+hysteriya@users.noreply.github.com>
2023-10-07 12:36:11 +0530

    nav

--- firststore/src/componenets/navbar/navbar.jsx
+++ firststore/src/componenets/navbar/navbar.jsx
@@ -3,8 +3,26 @@ import "./navbar.css";
 
 const navbar = () => {
   return (
-    <div>navbar</div>
-  )
+  <nav>
+    <a href="#" className='logo'>ricoz</a>
+    
+    <ul>
+        <li>
+          <a href='#'>About Us</a>
+        </li>
+        <li>
+          <a href='#'>Services</a>
+        </li>
+        <li>
+          <a href='#'>Courses</a>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
+    </ul>
+    </nav>
+  );
 }
+
 
 export default navbar
